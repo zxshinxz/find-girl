4c7d3f8c8f83eba4363bd712e6351a3c1ecc0313
Andy Shin <andy.yoonyong@gmail.com>
2014-03-17 07:42:11 +1300

    initial commit for start of project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('findGirlApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
